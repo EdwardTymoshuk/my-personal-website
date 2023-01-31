d6860d2a2e7f50cf98edc80ce2d94fb03a521eeb
Eduard Tymoshuk <edward_t@mbp-edward.home>
2023-01-31 20:01:26 +0100

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai';
+import { BiPlanet } from 'react-icons/bi';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () =>  (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
+          <BiPlanet size="3rem" /> <span>Edward Tymoshuk</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>        
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>        
+    </Div2>
+      <Div3>
+        <SocialIcons href="https://github.com/EdwardTymoshuk">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/eduard-tymoshuk-22b935198/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.facebook.com/eduard.tymoshuk">
+          <AiFillFacebook size="3rem"/>
+        </SocialIcons>
+      </Div3>
+    </Container>
+);
+
+export default Header;
