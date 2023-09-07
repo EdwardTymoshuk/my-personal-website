3fe888551f1581223bdffad75c53071255f55738
Edward <eduard.tymoshuk@gmail.com>
2023-09-07 15:00:06 +0200

    add new project, edit readme file

--- README.md
+++ README.md
@@ -1,14 +1,41 @@
-# Eduard Tymoshuk Personal Portfolio Web Page
+# My Personal Website
 
-My personal website where you can find more info about me, watch my CV, finished projects and my skills.
+## Overview
+Welcome to my personal website repository. This website serves as a portfolio and personal CV showcase, providing visitors with an overview of my skills, experiences, and contact information.
 
-## Stack
+![Project Screenshot](public/images/project-my-page.jpg)
 
-React, Next, Styled Components
+## Features
+- **Portfolio:** Explore my projects and view detailed information about each one.
+- **CV Download:** Download my CV (resume) directly from the website.
+- **Responsive Design:** The website is optimized for desktop, tablet, and mobile devices.
+- **Contact Form:** Reach out to me using the built-in contact form.
 
-## Screenshot
+## Technologies Used
+- **React:** A JavaScript library for building user interfaces.
+- **Next.js:** A framework for building React applications.
+- **Styled-components:** A CSS-in-JS library for styling components.
 
-![website screenshot](https://github.com/EdwardTymoshuk/my-personal-website/blob/master/public/images/project-4.jpg?raw=true)
+## Installation
+1. Clone this repository: `git clone https://github.com/EdwardTymoshuk/my-personal-website.git`
+2. Navigate to the project directory: `cd my-personal-website`
+3. Install dependencies: `npm install`
 
+## Usage
+- Start the development server: `npm run dev`
+- Access the website in your browser at `http://localhost:3000`
 
+## CV
+You can download my CV (resume) in PDF format directly from the website.
 
+## Contributing
+Contributions are welcome! If you'd like to contribute to this project, please follow the standard GitHub fork and pull request process.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Contact
+For any inquiries or feedback, please contact me:
+- Edward Tymoshuk
+- Email: [eduard.tymoshuk@gmail.com](mailto:eduard.tymoshuk@gmail.com)
+- GitHub: [https://github.com/EdwardTymoshuk](https://github.com/EdwardTymoshuk)
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,30 @@
 export const projects = [
   {
-    title: 'PostIt FullStack App',
-    description: "Full Stack web application which allows you to make posts, comment other's people posts, like it and delete your own posts. Application uses a Google Auth API for users login.",
-    image: '/images/project-postit.jpg',
-    tags: ['React', 'Next.js', 'Tailwind, Prisma, TypeScript'],
-    source: 'https://postit-react-next-app.vercel.app/',
-    visit: 'https://github.com/EdwardTymoshuk/postit-react-next-app',
+    title: 'E-commerce NextJs page (front end)',
+    description: "E-commerce-front is a sleek web application that serves as the frontend for an online e-commerce platform. It offers users an engaging shopping experience with features like product catalog browsing, efficient product search, convenient filtering by category or color, and a responsive shopping cart. Users can quickly find and purchase products in a modern and user-friendly interface. Active development ensures ongoing enhancements and improvements.",
+    image: '/images/project-ecommerce-next-app-front.jpg',
+    tags: ['Next.js', 'Styled Component', 'Prisma', 'MongoDB'],
+    source: 'projects/ecommerce-next-app-front',
+    visit: 'https://github.com/EdwardTymoshuk/https://github.com/dejwid/ecommerce-front',
     id: 0,
   },
   {
+    title: 'PostIt FullStack App',
+    description: "This is a dynamic full stack web application, designed for creating and managing digital sticky notes. It offers users a streamlined note-taking experience with features like creating, editing, commenting, likes or unlikes and organizing notes. The app provides real-time updates and synchronization, making it ideal for collaborative note-taking. Its responsive design ensures accessibility across various devices. ",
+    image: '/images/project-postit.jpg',
+    tags: ['React', 'Next.js', 'Tailwind', 'Prisma', 'TypeScript'],
+    source: 'https://postit-react-next-app.vercel.app/',
+    visit: 'https://github.com/EdwardTymoshuk/postit-react-next-app',
+    id: 1,
+  },
+  {
     title: 'My Personal Webpage (this website)',
-    description: "My fully responsiv personal website made with React library and Next.js framework. It uses a React Styled Components for app slyling.",
+    description: "This is a portfolio and CV (resume) showcase website designed to provide visitors with a comprehensive overview of my skills, experiences and contact information. This website serves as a professional online presence, featuring a responsive design, project portfolio, direct CV download, and a contact form for easy communication.",
     image: '/images/project-my-page.jpg',
     tags: ['React', 'Next.js', 'Styled Component'],
     source: '/',
     visit: 'https://github.com/EdwardTymoshuk/my-personal-website',
-    id: 1,
+    id: 2,
   },
   {
     title: 'Bon Bon Francais',
@@ -24,26 +33,8 @@ export const projects = [
       tags: ['HTML5', 'CSS3', 'JS', 'Bootstrap', 'MongoDB', 'Express'],
     source: '/projects/bonbonfrancais',
     visit: 'https://github.com/EdwardTymoshuk/Bonbonfrancais.online',
-    id: 2,
-  },
-  {
-    title: 'Metaverse Website',
-    description:"Modern React and Next.js page. Project represents only front end layer without functionality.",
-    image: '/images/project-metaverse.jpg',
-    tags: ['React', 'Next.js', 'Tailwind', 'Framer Motion'],
-    source: 'projects/metaversus-next-react-app',
-    visit: 'https://github.com/EdwardTymoshuk/metaversus-next-react-app',
     id: 3,
-  },
-  {
-    title: 'Snake Game',
-    description: "My graduation project at Javascript course. It represents a simple game 'Snake' and is made with HTML5, CSS3 and Vanila Js. During this project I got a deeper knowledge of JS, wich helps me in my future projects.",
-    image: '/images/project-snake.jpg',
-    tags: ['HTML5', 'CSS3', 'JS'],
-    source: 'projects/snake',
-    visit: 'https://github.com/EdwardTymoshuk/SnakeApp',
-    id: 4,
-  },
+  }
 ];
 
 export const TimeLineData = [
