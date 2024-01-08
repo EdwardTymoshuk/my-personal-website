727126df073c9bacf6de0d656a2f3028ce576030
Edward <eduard.tymoshuk@gmail.com>
2024-01-08 16:59:49 +0100

    add new project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,21 @@
 export const projects = [
-  {
-    title: 'E-commerce NextJs page (front end)',
-    description: "E-commerce-front is a sleek web application that serves as the frontend for an online e-commerce platform. It offers users an engaging shopping experience with features like product catalog browsing, efficient product search, convenient filtering by category or color, and a responsive shopping cart. Users can quickly find and purchase products in a modern and user-friendly interface. Active development ensures ongoing enhancements and improvements.",
+    {
+    title: 'E-commerce page',
+    description: "E-commerce page is a sleek web application that serves as the frontend for an online e-commerce platform. It offers users an engaging shopping experience with features like product catalog browsing, efficient product search, convenient filtering by category or color, and a responsive shopping cart. Users can quickly find and purchase products in a modern and user-friendly interface. Active development ensures ongoing enhancements and improvements.",
     image: '/images/project-ecommerce-next-app-front.png',
     tags: ['Next.js', 'Styled Component', 'Prisma', 'MongoDB'],
     source: 'https://e-commerce-front-opal.vercel.app/',
     visit: 'https://github.com/EdwardTymoshuk/e-commerce-front',
     id: 0,
+  },
+  {
+    title: 'E-commerce page admin panel',
+    description: "This is a dynamic admin panel crafted to streamline the management of e-commerce page. It empowers you with the ability to effortlessly add, edit, and categorize products. The panel provides a comprehensive view of all processed orders, while the homepage offers various statistics related to sales and product performance. Application also allows to login using Google account or email/password.",
+    image: '/images/project-ecommerce-admin-panel.jpg',
+    tags: ['Next.js', 'Tailwind', 'MongoDB'],
+    source: 'https://e-commerce-admin-panel-ten.vercel.app/',
+    visit: 'https://github.com/EdwardTymoshuk/e-commerce-admin-panel',
+    id: 1,
   },
   {
     title: 'PostIt FullStack App',
@@ -15,7 +24,7 @@ export const projects = [
     tags: ['React', 'Next.js', 'Tailwind', 'Prisma', 'TypeScript'],
     source: 'https://postit-react-next-app.vercel.app/',
     visit: 'https://github.com/EdwardTymoshuk/postit-react-next-app',
-    id: 1,
+    id: 2,
   },
   {
     title: 'My Personal Webpage (this website)',
@@ -24,7 +33,7 @@ export const projects = [
     tags: ['React', 'Next.js', 'Styled Component'],
     source: '/',
     visit: 'https://github.com/EdwardTymoshuk/my-personal-website',
-    id: 2,
+    id: 3,
   },
   {
     title: 'Bon Bon Francais',
@@ -33,7 +42,7 @@ export const projects = [
       tags: ['HTML5', 'CSS3', 'JS', 'Bootstrap', 'MongoDB', 'Express'],
     source: '/projects/bonbonfrancais',
     visit: 'https://github.com/EdwardTymoshuk/Bonbonfrancais.online',
-    id: 3,
+    id: 4,
   }
 ];
 
@@ -44,4 +53,5 @@ export const TimeLineData = [
   { year: 2019, text: 'Finished a JavaScript + React/Redux cours. Started making simple projects using learned technologies', },
   { year: 2021, text: 'Still studying and making projects. Got some basic knowledge of TypeScript, Node.js, MongoDB, Express', },
   { year: 2022, text: 'Developed my React skills (Hooks, Context API etc.), got acquainted SCSS and Tailwind tools. Started work as Web Developer at Crowbox company', },
+  { year: 2024, text: 'I am deepening my knowledge in such technologies as Next.Js, Tailwind and MongoDB and use them to create projects', },
 ];
